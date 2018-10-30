058eb3f16a81658f2f2823fbb8a3b26e4c5f68c7
Hayden Eastwood <beastflow@gmail.com>
2018-10-31 00:44:13 +0200

    changes to graph error

--- new-customers.py
+++ new-customers.py
@@ -11,7 +11,8 @@
 from sklearn import linear_model
 import pandas as pd
 import calendar
-import matplotlib as plt
+import matplotlib.pyplot as plt
+
 
 print " "
 print " * * * * * * * * * * * * * * * * *"
@@ -27,8 +28,8 @@ with open('challenge.json', 'r') as f:
 
 print " -- Setting parameters"
 start_year = 2008
-end_year = 2019
-
+end_year = 2018
+month_range = 12
 
 data.register_date = pd.to_datetime(data.register_date, format='%Y-%m-%d')
 data.set_index('register_date', inplace=True)
@@ -39,14 +40,14 @@ month_count = 0
 uniques_per_month = []
 uniques_per_month_count = []
 uniques_per_month_count = []
-month_range = 12
+
 updated_set = set()
 
 print " -- Finding cumulative users per month"
 for year in range(start_year, end_year):
 	usersArray = []
 	if year == 2018:
-		month_range = 6
+		month_range = 6 # make sure we only look at last 6 months since data collection stopped mid month in following months
 	for month in range(0, month_range):
 		date_string_base = str(year) + '-' + str(month + 1) 
 		start_date = date_string_base + '-01' 
@@ -66,7 +67,7 @@ lm = linear_model.LinearRegression(fit_intercept=True)
 model = lm.fit(indexes,cumsum[0])
 
 sign_up_rate = model.coef_
-print "     -- Customer sign up rate (regression calculated): " + str(sign_up_rate)
+print "     -- Customer sign up rate (regression calculated): " + str(sign_up_rate[0])
 
 predictions = lm.predict(indexes)
 plt.plot(predictions)
